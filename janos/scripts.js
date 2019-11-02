89ef4f2e5e2e70924396e1d5b9a6434574d1a96d
Janos Torba <janos@torba.hu>
2019-11-02 23:19:01 +0100

    dialog nem eleg szeles

--- janos/scripts.js
+++ janos/scripts.js
@@ -24,6 +24,43 @@ $( window ).on( "popstate", function() {
 
 xCache = {};
 
+dialogOptions = {
+    create: function(event, ui) {
+        windowResize();
+        $(this).parent().css( "top", 0 ).css("left", 0);
+        $(this).dialog("option", "maxHeight", $(window).height() - $(".taskbar").height() ).dialog("option", "height", $(this).dialog("option", "maxHeight"));
+        $(this).dialog("option", "maxWidth", $(window).width() ).dialog("option", "width", $(this).dialog("option", "maxwidth"));
+    },
+    close: function(event, ui){
+        $(this).dialog('destroy').remove();
+    },
+    // show: { effect: "blind", duration: 1000 },
+    // hide: "puff",
+    top: 10,
+    left: 1
+    // height: 200
+};
+
+dialogExtendOptions = {
+    "closable" : true,
+    "maximizable" : true,
+    "minimizable" : true,
+    "collapsable" : false,
+    "dblclick" : "maximize", // set action on double click. false, 'maximize', 'minimize', 'collapse'
+    // "titlebar" : "transparent", // false, 'none', 'transparent'
+    "minimizeLocation" : "right", // sets alignment of minimized dialogues
+    "icons" : { // jQuery UI icon class
+        "close" : "ui-icon-circle-close",
+        "maximize" : "ui-icon-circle-plus",
+        "minimize" : "ui-icon-circle-minus",
+        "collapse" : "ui-icon-triangle-1-s",
+        "restore" : "ui-icon-bullet"
+    },
+    "load" : function(evt, dlg){
+        // $(this).
+    }
+}
+
 function loadFolder() {
     url = window.location.href;
     hash = window.location.hash;
@@ -42,6 +79,7 @@ function loadFolder() {
     xslt = xCache["desktop.xslt"];
     xml = xCache[hash];
 
+
     if  (xslt && url.match(/.xml$/)) {
         if  (xml) {
             $('.desktop').html(transform(xml, xslt));
@@ -54,21 +92,24 @@ function loadFolder() {
     } else {
         if (url.match(/torba.hu/) && url.match(/txt$/)) {
             $.get( hash, function( text ) {
-                $('.desktop').html('<div class="window"><div class="title">'+hash+'</div><pre class="text-reader window-minus-title">'+text+'</pre></div>');
-                $( "body" ).append('<div id="dialog2" title="Basic dialog2"><p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the "x" icon.</p></div>');
-                windowResize();
+                $( "body" ).append('<div class="parbeszedablak" title="'+hash+'"><pre>'+text+'</pre></div>');
+                $( ".parbeszedablak" ).dialog(dialogOptions).dialogExtend(dialogExtendOptions);
             });
         } else if (url.match(/torba.hu/) && url.match(/(png|gif|jpg|jpeg)$/)) {
-            $('.desktop').html('<div class="window"><div class="title">'+hash+'</div><div class="image-viewer window-minus-title"><img class="viewed-image" src="'+hash+'" /></div></div>');
-            $('.viewed-image').load( function(){ windowResize() });
+            $( "body" ).append('<div class="parbeszedablak" title="'+hash+'"><img class="viewed-image" src="'+hash+'" /></div>');
+            $( ".parbeszedablak" ).dialog(dialogOptions).dialogExtend(dialogExtendOptions);
         } else if (url.match(/torba.hu/) && url.match(/.lightbox$/)) {
             hash = hash.replace(/.lightbox$/, "");
             $.get( hash, function( text ) {
-                $('.hidden-window').html(text);
-                $('.desktop').html('<div class="window"><div class="title">'+hash+'</div><div class="gallery window-minus-title"></div></div>');
-                $(".hidden-window a").each( function(i, a){ h="vicc/"+$(a).attr("href"); if (h.match(/(png|gif|jpg|jpeg)$/i)) { $(".gallery").append('<a data-lightbox="gallery" href="'+h+'"><img class="thumbnail" src="'+h+'" /></a>');} });
-                windowResize();
-	            // $('.thumbnail-image').load( function(){ windowResize() });
+                $('.hidden').html(text);
+                $('body').append('<div class="parbeszedablak gallery" title="'+hash+' galeria"></div>');
+                $(".hidden a").each( function(i, a){
+                    h=hash+"/"+$(a).attr("href");
+                    if (h.match(/(png|gif|jpg|jpeg)$/i)) {
+                        $(".gallery").append('<a data-lightbox="gallery" href="'+h+'"><img class="thumbnail" src="'+h+'" /></a>');
+                    }
+                });
+                $( ".parbeszedablak" ).dialog(dialogOptions).dialogExtend(dialogExtendOptions);
             });
         } else {
             $.get( "desktop.xslt", function( xslt ) {
@@ -108,8 +149,11 @@ function transform(xml, xsl) {
  * Resize taskbar buttons depending on the screensize
  */
 function windowResize() {
+    wh = $(window).height();
+    ww = $(window).width();
 
-//    $('.window').each(function(i, win){ $(win).attr("data-before", win.title); });
+    /*
+     * Resize old own created windows
     $('.window').css('max-height', '');
     $('.window-minus-title').css('max-height', '');
     if ($('.window').height() + 110 >= $(window).height() ){
@@ -119,7 +163,32 @@ function windowResize() {
     $('.window').css('max-width', '');
     if ($('.window').width() + 65 >= $(window).width() ){
         $('.window').css('max-width', ($(window).width() - 65)+'px');
-    }
+    }*/
+
+    $( ".parbeszedablak" ).each( function(i, parbeszedablak) {
+        $parbeszedablak = $(parbeszedablak);
+        //$parbeszedablak.dialog("option", "maxWidth", ww);
+        //$parbeszedablak.dialog("option", "maxHeight", wh);
+        //if (wh > $parbeszedablak.height() ){
+            //$parbeszedablak.dialog( "option", "height", "auto" );
+        //}
+        //if (ww <= $parbeszedablak.width() ){
+            //$parbeszedablak.dialog( "option", "width", "auto" );
+        //}
+        if (wh <= $parbeszedablak.height() ){
+            //$parbeszedablak.dialog( "option", "height", wh-70 );
+            //$parbeszedablak.height(wh-70);
+        }
+        if (ww <= $parbeszedablak.width() ){
+            //$parbeszedablak.dialog( "option", "width", ww-30 );
+            //$parbeszedablak.width(ww-30);
+        }
+        // $( ".selector" ).dialog( "option", "resizable", true );
+    });
+
+    /*
+     * Resize Taskbar
+     */
     if ($(window).width() < 350) {
 		$('.taskbar .button').filter(".middle").hide();
 	} else {
